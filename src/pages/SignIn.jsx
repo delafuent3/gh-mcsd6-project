53807547e5ebac164367817177943225872f56ae
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-24 01:00:27 +1200

    Task 4e - Add CSS to LINKS (Register, Forgot password), SIGN IN button and OR label

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -31,7 +31,7 @@ export default function SignIn() {
                         />
 
                         {/* ------------------------------------------------------------ INPUT - PASSWORD ---------- */}
-                        <div>
+                        <div className="relative mb-6">
                             <input
                                 type="password"
                                 id="password"
@@ -41,33 +41,43 @@ export default function SignIn() {
                             />
                         </div>
 
+
+                        {/* ------------------------------------------------------------------------------- LINKS ---------- */}
+                        <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+                            <p className="mb-6">
+                                Don't have a account?
+                                <Link
+                                    className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                                >
+                                    Register
+                                </Link>
+                            </p>
+                            <p>
+                                <Link
+                                    className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                                >
+                                    Forgot password?
+                                </Link>
+                            </p>
+                        </div>
+
+                        {/* -------------------------------------------------------------------- BUTTON - SIGN IN ---------- */}
+                        <button
+                            className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+                        >
+                            Sign in
+                        </button>
+
+                        {/* ---------------------------------------------------------------------- PARAGRAPH - OR ---------- */}
+                        <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+                            <p className="text-center font-semibold mx-4">OR</p>
+                        </div>
+
+
                     </form>
                 </div>
 
-                {/* ------------------------------------------------------------------------------- LINKS ---------- */}
-                <div>
-                    <p>
-                        Don't have a account?
-                        <Link>
-                            Register
-                        </Link>
-                    </p>
-                    <p>
-                        <Link>
-                            Forgot password?
-                        </Link>
-                    </p>
-                </div>
 
-                {/* -------------------------------------------------------------------- BUTTON - SIGN IN ---------- */}
-                <button>
-                    Sign in
-                </button>
-
-                {/* ---------------------------------------------------------------------- PARAGRAPH - OR ---------- */}
-                <div>
-                    <p>OR</p>
-                </div>
             </div>
         </section>
     )
