ed8db0eaa7f7f49fc940490ebffc59e857d95587
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-23 22:37:26 +1200

    Task 3b - Add Logo and Menu Items to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
+// Import images
+import logo from "../images/header_logo.jpg"
+
 export default function Header() {
     return (
-        <div>Header</div>
+        <div>
+            <header>
+                {/* -------------------------------------------------------------------------------- LOGO ---------- */}
+                <div>
+                    <img src={logo} alt="Logo"/>
+                </div>
+
+                {/* -------------------------------------------------------------------------------- MENU ---------- */}
+                <div>
+                    <ul>
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            Offers
+                        </li>
+                        <li>
+                            Sign In
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
     )
 }
