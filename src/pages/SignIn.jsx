79d65ac798cc09d019dae7461cad4d8e95ea722d
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-23 23:40:41 +1200

    Task 4a - Add CSS for all the pages in the application

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,8 @@
 export default function SignIn(){
     return (
-        <div> Sign In </div>
+        <section>
+            <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+        </section>
     )
 }
+
