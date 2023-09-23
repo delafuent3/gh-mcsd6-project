e31dee857e2274bdec8aedd4a584376e5e71a999
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-24 00:22:54 +1200

    Task 4c - Add CSS to main DIV and Image

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -9,10 +9,12 @@ export default function SignIn() {
         <section>
             <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
 
-            <div>
+            {/* ------------------------------------------------------------------------------ DIV - MAIN ---------- */}
+            <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+
                 {/* ------------------------------------------------------------------------------- IMAGE ---------- */}
-                <div>
-                    <img src={keyImage} alt="key"/>
+                <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+                    <img src={keyImage} alt="key" className="w-full rounded-2xl"/>
                 </div>
 
                 {/* -------------------------------------------------------------------------------- FORM ---------- */}
