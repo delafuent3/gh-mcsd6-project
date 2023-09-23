98b4403078f800424038bd3de0389997d656f28d
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-24 00:11:42 +1200

    Task 4b - Add Key image and Form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,63 @@
-export default function SignIn(){
+// Import images
+import keyImage from "../images/login_key.jpg"
+
+// Import Libraries
+import {Link} from "react-router-dom";
+
+export default function SignIn() {
     return (
         <section>
             <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+
+            <div>
+                {/* ------------------------------------------------------------------------------- IMAGE ---------- */}
+                <div>
+                    <img src={keyImage} alt="key"/>
+                </div>
+
+                {/* -------------------------------------------------------------------------------- FORM ---------- */}
+                <div>
+                    <form>
+                        {/* --------------------------------------------------------------- INPUT - EMAIL ---------- */}
+                        <input
+                            type="email"
+                        />
+
+                        {/* ------------------------------------------------------------ INPUT - PASSWORD ---------- */}
+                        <div>
+                            <input
+                                type="password"
+                            />
+                        </div>
+
+                    </form>
+                </div>
+
+                {/* ------------------------------------------------------------------------------- LINKS ---------- */}
+                <div>
+                    <p>
+                        Don't have a account?
+                        <Link>
+                            Register
+                        </Link>
+                    </p>
+                    <p>
+                        <Link>
+                            Forgot password?
+                        </Link>
+                    </p>
+                </div>
+
+                {/* -------------------------------------------------------------------- BUTTON - SIGN IN ---------- */}
+                <button>
+                    Sign in
+                </button>
+
+                {/* ---------------------------------------------------------------------- PARAGRAPH - OR ---------- */}
+                <div>
+                    <p>OR</p>
+                </div>
+            </div>
         </section>
     )
 }
