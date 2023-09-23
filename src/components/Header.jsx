e303bb7daf3c76ad7ab391438df60897eca373bc
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-23 23:17:39 +1200

    Task 3c3 - Add Navigation Functionality to Menu Items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,12 @@ import logo from "../images/header_logo.jpg"
 
 // Import Hooks
 import {useLocation} from "react-router";
+import {useNavigate} from "react-router";
 
 export default function Header() {
     // ------------------------------------------------------------------------------------------------ HOOKS ----------
     const location = useLocation();
+    const navigate = useNavigate();
 
     // ---------------------------------------------------------------------------- FUNCTION - pathMatchRoute ----------
     function pathMatchRoute(route){
@@ -20,21 +22,35 @@ export default function Header() {
             <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
                 {/* -------------------------------------------------------------------------------- LOGO ---------- */}
                 <div>
-                    <img src={logo} alt="Logo" className="h-5 cursor-pointer"/>
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        className="h-5 cursor-pointer"
+                        onClick={()=>navigate("/")}
+                    />
                 </div>
 
                 {/* -------------------------------------------------------------------------------- MENU ---------- */}
                 <div>
                     <ul className="flex space-x-10">
-                        {/*<li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  ${pathMatchRoute("/") && "text-black border-b-red-500"}`}>*/}
-                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}>
+                        <li
+                            className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}
+                            onClick={()=>navigate("/")}
+                        >
                             Home
                         </li>
 
-                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}>
+                        <li
+                            className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}
+                            onClick={()=>navigate("/offers")}
+                        >
                             Offers
                         </li>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/sign-in") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}>
+
+                        <li
+                            className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/sign-in") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}
+                            onClick={()=>navigate("/sign-in")}
+                        >
                             Sign In
                         </li>
                     </ul>
