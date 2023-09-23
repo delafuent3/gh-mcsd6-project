b32fb73277b862173a5746a5754d768520818da4
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-23 23:07:28 +1200

    Task 3c2 - Apply style to the menu items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 // Import images
 import logo from "../images/header_logo.jpg"
 
+// Import Hooks
+import {useLocation} from "react-router";
+
 export default function Header() {
+    // ------------------------------------------------------------------------------------------------ HOOKS ----------
+    const location = useLocation();
+
+    // ---------------------------------------------------------------------------- FUNCTION - pathMatchRoute ----------
+    function pathMatchRoute(route){
+        if(route === location.pathname) {
+            return true
+        }
+    }
+
     return (
         <div className="bg-white border-b shadow-sm sticky top-0 z-40">
             <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -13,13 +26,15 @@ export default function Header() {
                 {/* -------------------------------------------------------------------------------- MENU ---------- */}
                 <div>
                     <ul className="flex space-x-10">
-                        <li>
+                        {/*<li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  ${pathMatchRoute("/") && "text-black border-b-red-500"}`}>*/}
+                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}>
                             Home
                         </li>
-                        <li>
+
+                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}>
                             Offers
                         </li>
-                        <li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/sign-in") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent" }`}>
                             Sign In
                         </li>
                     </ul>
