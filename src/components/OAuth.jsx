ff7250afa7e566e5e50825aaadb2135493e90606
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-24 16:31:24 +1300

    Task 6c - Apply CSS to Button (OAuth component)

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -3,8 +3,8 @@ import {FcGoogle} from "react-icons/fc";
 
 export default function OAuth() {
     return (
-        <button>
-            <FcGoogle/>
+        <button className="bg-red-700 flex w-full items-center justify-center text-white px-7  py-3 uppercase font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+            <FcGoogle className="text-2xl bg-white rounded-full mr-2"/>
             Continue with Google
         </button>
     )
