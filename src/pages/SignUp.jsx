668b8f37032ba885eae39073714d3a71d491df13
Claudio de la Fuente <delafuente@outlook.com.au>
2023-09-25 15:04:28 +1300

    Task 9b - Navigate functionality on SignUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -106,6 +106,7 @@ export default function SignUp() {
                                 Have an account?
                                 <Link
                                     className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                                    to="/sign-in"
                                 >
                                     Sign in
                                 </Link>
@@ -113,6 +114,7 @@ export default function SignUp() {
                             <p>
                                 <Link
                                     className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                                    to="/forgot-password"
                                 >
                                     Forgot password?
                                 </Link>
