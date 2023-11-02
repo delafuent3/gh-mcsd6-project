14d2850fcc91dcce123a222c4943adce95104e40
Claudio de la Fuente <delafuente@outlook.com.au>
2023-11-02 17:54:18 +1300

    Task 35 - ListingItem finished - CSS

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -194,26 +194,17 @@ export default function Profile() {
                         <h2 className="text-2xl text-center font-semibold mb-6">
                             My Listings
                         </h2>
-                        <ul>
+                        <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
                             {listings.map((listing) => (
                                 <ListingItem
                                     key={listing.id}
                                     id={listing.id}
                                     listing={listing.data}
+                                    // onDelete={() => onDelete(listing.id)}
+                                    // onEdit={() => onEdit(listing.id)}
                                 />
                             ))}
                         </ul>
-                        {/*<ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">*/}
-                        {/*    {listings.map((listing) => (*/}
-                        {/*        <ListingItem*/}
-                        {/*            key={listing.id}*/}
-                        {/*            id={listing.id}*/}
-                        {/*            listing={listing.data}*/}
-                        {/*            onDelete={() => onDelete(listing.id)}*/}
-                        {/*            onEdit={() => onEdit(listing.id)}*/}
-                        {/*        />*/}
-                        {/*    ))}*/}
-                        {/*</ul>*/}
                     </>
                 )}
             </div>
